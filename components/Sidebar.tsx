83dd31290cbd0a2cb2a35e7ab552e6ae74acb86b
Jeim05 <lopezvegajeimmy@gmail.com>
2024-10-17 22:48:29 -0600

    Mapping icons

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,12 +1,16 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faAngleLeft, faChartLine } from '@fortawesome/free-solid-svg-icons'
+import { faAngleLeft} from '@fortawesome/free-solid-svg-icons'
+import { faChartLine } from '@fortawesome/free-solid-svg-icons'
 import Link from 'next/link'
 import React from 'react'
+import { ITEMS_MENU } from '@/constants'
+import { library } from '@fortawesome/fontawesome-svg-core'
+library.add(faChartLine);
 
 function Sidebar() {
   return (
     <aside className='h-screen'>
-      <nav className='h-full flex flex-col bg-white border-r shadow-sm '>
+      <nav className='h-full flex flex-col bg-white border-r shadow-sm w-60'>
         <div className='p-4 pb-2 flex justify-between items-center'>
           <Link href='/' className='font-bold'>
             FINANCE APP
@@ -16,11 +20,13 @@ function Sidebar() {
           </button>
         </div>
         <ul className='flex-1 px-3'>
-          <li className={`relative flex items-center py-2 px-3 my-1 font-medium rounded-md`}>
+          {ITEMS_MENU.map((item)=>(
+            <li className={`relative flex items-center py-2 px-3 my-1 font-medium rounded-md`}>
             <Link href="/">
-              <FontAwesomeIcon icon={faChartLine} /> Dashboard
+              <FontAwesomeIcon icon={item.icon} /> {item.label}
             </Link>
           </li>
+          ))} 
         </ul>
       </nav>
     </aside>
---
+++ constants/index.ts
@@ -0,0 +1,5 @@
+
+export const ITEMS_MENU=[
+    {href:'/', key:'home', label:'Dashboard', icon:['fas','fa-chart-line']},
+    {href:'/', key:'transactions', label:'Transacciones', icon:['fas','fa-chart-line']},
+]
