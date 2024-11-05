08876e18b6ad0ba0ca0d531b6b66651b184871ca
Jeim05 <lopezvegajeimmy@gmail.com>
2024-11-04 22:32:55 -0600

    sidebar links

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -10,19 +10,22 @@ const Sidebar = () => {
         <section className='sidebar'>
             <nav className='flex flex-col gap-4 '>
                 <Link href="/" className='mb-12 cursor-pointer items-center gap-2'>
-                    <h1 className='bg-gradient-to-r from-indigo-500 to-blue-600 bg-clip-text text-transparent font-bold text-2xl'>
-                        MyFinanceApp
+                    <h1 className='sidebar-logo bg-gradient-to-r from-indigo-500 to-blue-600 bg-clip-text text-transparent font-bold text-2xl'>
+                        FinApp
+                    </h1>
+                    <h1 className='sidebar-logo bg-gradient-to-r from-indigo-500 to-blue-600 bg-clip-text text-transparent font-bold text-2xl'>
+                        FApp
                     </h1>
                 </Link>
                 {sidebarLinks.map((item) => {
                     const isActive = pathname == item.route || pathname.startsWith(`${item.route}/`);
 
                     return (
-                        <Link key={item.label} href={item.route} className={`sidebar-link ${isActive ? 'bg-gradient-to-r from-indigo-500 to-blue-500 text-white' : ''}`}>
+                        <Link key={item.label} href={item.route} className={`text-black sidebar-link ${isActive ? 'bg-gradient-to-r from-indigo-500 to-blue-500 text-white' : ''}`}>
                             <div className='relative size-6'>
-                                <Image src={item.icon} width={20} height={20} alt={item.label} fill className='brightness-[3] invert-0' />
+                                <Image src={item.icon} fill alt={item.label} className={`brightness-50 ${isActive ? 'brightness-200 invert' : '' }`} />
                             </div>
-                            <p className='>
+                            <p className='sidebar-label'>
                                 {item.label}
                             </p>
                         </Link>
