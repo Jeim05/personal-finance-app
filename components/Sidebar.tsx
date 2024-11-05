b7bace4ac2ef33a8b304c0cab2c92d3426e1b111
Jeim05 <lopezvegajeimmy@gmail.com>
2024-11-04 21:16:33 -0600

    sidebar

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -18,9 +18,13 @@ const Sidebar = () => {
                     const isActive = pathname == item.route || pathname.startsWith(`${item.route}/`);
 
                     return (
-                        <Link key={item.label} href={item.route} className={`text-black flex gap-2 items-center px-2 py-2 rounded-sm ${isActive ? 'bg-gradient-to-r from-indigo-500 to-blue-500 text-white' : ''}`}>
-                            <Image src={item.icon} width={20} height={20} alt='icon' className='text-red-800' />
-                            {item.label}
+                        <Link key={item.label} href={item.route} className={`sidebar-link ${isActive ? 'bg-gradient-to-r from-indigo-500 to-blue-500 text-white' : ''}`}>
+                            <div className='relative size-6'>
+                                <Image src={item.icon} width={20} height={20} alt={item.label} fill className='brightness-[3] invert-0' />
+                            </div>
+                            <p className='>
+                                {item.label}
+                            </p>
                         </Link>
                     )
                 })}
