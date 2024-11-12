b1e51123c133855310b49fa7118485c9121da5f4
Jeim05 <lopezvegajeimmy@gmail.com>
2024-11-11 21:10:33 -0600

    Icons for toggle sidebar

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -17,7 +17,7 @@ const Sidebar = () => {
         <section className={`sidebar ${isExpanded ? 'w-64' : 'w-20'}`}>
             <nav className='flex flex-col gap-4 relative'>
                 <Link href="/" className='mb-10 text-center cursor-pointer items-center gap-2'>
-                    <h1 className={`sidebar-logo bg-gradient-to-r from-indigo-500 to-blue-600 bg-clip-text text-transparent font-bold text-2xl ${!isExpanded && 'hidden'}`}>
+                    <h1 className={`sidebar-logo  ${!isExpanded && 'hidden'}`}>
                         FiApp
                     </h1>
                 </Link>
@@ -25,7 +25,7 @@ const Sidebar = () => {
                     const isActive = pathname == item.route || pathname.startsWith(`${item.route}/`);
 
                     return (
-                        <Link key={item.label} href={item.route} className={`text-black sidebar-link ${isActive ? 'bg-gradient-to-r from-indigo-500 to-blue-500 text-white' : ''}`}>
+                        <Link key={item.label} href={item.route} className={`sidebar-link ${isActive ? 'bg-gradient-to-r from-indigo-500 to-blue-500 text-white' : ''}`}>
                             <div className='relative size-6'>
                                 <Image src={item.icon} fill alt={item.label} className={`brightness-50 ${isActive ? 'brightness-200 invert' : '' }`} />
                             </div>
@@ -35,8 +35,13 @@ const Sidebar = () => {
                         </Link>
                     )
                 })}
-                <button onClick={toggleSidebar} className="toggle-button absolute top-4 -right-6">
-                    <span className="arrow">{isExpanded ? '←' : '→'}</span>
+                <button onClick={toggleSidebar} className="toggle-button">
+                    {isExpanded ? (
+                        <Image src="/images/icons-sidebar/chevron-left-solid.svg" width={10} height={10} alt='close' />
+                    ):(
+                        <Image src="/images/icons-sidebar/chevron-right-solid.svg" width={10} height={10} alt='open' />
+                    )}
+                    
                 </button>
             </nav>
         </section>
