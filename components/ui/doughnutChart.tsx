a88b5fabbe35b412d10268b2afbafaca3322ca6c
Jeim05 <lopezvegajeimmy@gmail.com>
2024-11-13 21:59:50 -0600

    Update doughnutChart.tsx
    Mostrar datos de manera dinamica

--- components/ui/doughnutChart.tsx
+++ components/ui/doughnutChart.tsx
@@ -5,16 +5,37 @@ import { Doughnut, Bar } from "react-chartjs-2";
 ChartJS.register(ArcElement, Tooltip, Legend, CategoryScale, LinearScale, BarElement);
 
 const meses = [
-  'Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo'
+  {
+    'nombre': 'Enero',
+    'valor': 50
+  },
+  {
+    'nombre': 'Febrero',
+    'valor': 30
+  },
+  {
+    'nombre': 'Marzo',
+    'valor': 45
+  },
+  {
+    'nombre': 'Abril',
+    'valor': 40
+  },
+  {
+    'nombre': 'Mayo',
+    'valor': 86
+  },
 ]
 
 const DoughnutChart = ({accounts}:DoughnutChartProps) => {
+  const labels = meses.map(mes => mes.nombre);
+  const values = meses.map(mes => mes.valor);
    
   const data = {
-    labels: meses,
+    labels: labels,
     datasets: [{
       label: 'My First Dataset',
-      data: [65, 59, 80, 81, 56],
+      data: values,
       backgroundColor: [
         'rgba(255, 99, 132, 0.2)',
         'rgba(255, 159, 64, 0.2)',
