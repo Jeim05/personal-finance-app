287fa28043b573a7e1dfdce64723bd33e614a259
Jeim05 <lopezvegajeimmy@gmail.com>
2024-11-13 21:46:37 -0600

    Update doughnutChart.tsx

--- components/ui/doughnutChart.tsx
+++ components/ui/doughnutChart.tsx
@@ -1,23 +1,44 @@
 "use client"
-import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
-import { Doughnut } from "react-chartjs-2";
+import { Chart as ChartJS, ArcElement, Tooltip, Legend, CategoryScale, LinearScale, BarElement } from "chart.js";
+import { Doughnut, Bar } from "react-chartjs-2";
 
-ChartJS.register(ArcElement, Tooltip, Legend);
+ChartJS.register(ArcElement, Tooltip, Legend, CategoryScale, LinearScale, BarElement);
+
+const meses = [
+  'Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo'
+]
 
 const DoughnutChart = ({accounts}:DoughnutChartProps) => {
+   
   const data = {
-    datasets: [
-      {
-        label: 'Banks',
-        data: [1250, 2500, 3750],
-        backgroundColor: ['#0747b6', '#2265d8', '#2f91fa']
-      }
-    ],
-    labels: ['Bank 1', 'Bank 2', 'Bank 3']
-  }
+    labels: meses,
+    datasets: [{
+      label: 'My First Dataset',
+      data: [65, 59, 80, 81, 56],
+      backgroundColor: [
+        'rgba(255, 99, 132, 0.2)',
+        'rgba(255, 159, 64, 0.2)',
+        'rgba(255, 205, 86, 0.2)',
+        'rgba(75, 192, 192, 0.2)',
+        'rgba(54, 162, 235, 0.2)',
+        'rgba(153, 102, 255, 0.2)',
+        'rgba(201, 203, 207, 0.2)'
+      ],
+      borderColor: [
+        'rgb(255, 99, 132)',
+        'rgb(255, 159, 64)',
+        'rgb(255, 205, 86)',
+        'rgb(75, 192, 192)',
+        'rgb(54, 162, 235)',
+        'rgb(153, 102, 255)',
+        'rgb(201, 203, 207)'
+      ],
+      borderWidth: 1
+    }]
+  };
 
   return (
-    <Doughnut data={data} />
+    <Bar data={data}/>
   )
 }
 
