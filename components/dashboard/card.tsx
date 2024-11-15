59d72be1479f8707ca4c16efba471440cd5d3306
Jeim05 <lopezvegajeimmy@gmail.com>
2024-11-14 22:01:35 -0600

    updated cards

--- components/dashboard/card.tsx
+++ components/dashboard/card.tsx
@@ -2,15 +2,15 @@ import { CardProps } from '@/types'
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
-export default function Card({title,amount, icon, icon_variant, variant}: CardProps) {
+export default function Card({ title, amount, icon, icon_variant, variant }: CardProps) {
   return (
-    <div className={`flex flex-row py-4 px-5 rounded-lg items-center w-full h-36 ${variant}`}>
-      <div className='basis-3/4'>
-        <h3 className='font-bold text-gray-400 py-1 mb-4'>{title}</h3>
+    <div className={`flex flex-row px-5 rounded-lg items-center w-full h-36 ${variant}`}>
+      <div className='basis-3/4 flex flex-col gap-4 content-start'>
+        <h3 className='font-semibold text-gray-400 py-1 -mt-3 text-sm'>{title}</h3>
         <p className='font-bold text-2xl'>₡{amount}</p>
       </div>
       <div className={`basis-1/4 text-4xl text-end ${icon_variant}`}>
-      <FontAwesomeIcon icon={icon} />
+        <FontAwesomeIcon icon={icon} />
       </div>
     </div>
   )
--- components/dashboard/mainCards.tsx
+++ components/dashboard/mainCards.tsx
@@ -5,10 +5,10 @@ import { faHandHoldingDollar, faMoneyBillTransfer, faWallet, faMagnifyingGlassDo
 export default function MainCards() {
   return (
     <div className='container flex flex-row gap-4 justify-between'>
-        <Card title='Ingreso mensual' amount={500000} icon={faHandHoldingDollar} icon_variant='text-[#]' variant='bg-[#DAE9E8]' />
-        <Card title='Egreso' icon={faMoneyBillTransfer} amount={60000} icon_variant='' variant='bg-[#F9E0E1]' />
-        <Card title='Saldo disponible' amount={50000} icon={faWallet} icon_variant='' variant='bg-[#D8E2FC]' />
-        <Card title='Presupuesto Mensual' amount={200000} icon={faMagnifyingGlassDollar} icon_variant='' variant='bg-[#CDE7FC]' />
+        <Card title='Ingreso mensual' amount={500000.00} icon={faHandHoldingDollar} icon_variant='text-[#86A1A0]' variant='bg-[#DAE9E8]' />
+        <Card title='Egreso Actual Mensual' icon={faMoneyBillTransfer} amount={600000} icon_variant='text-[#AD9596]' variant='bg-[#F9E0E1]' />
+        <Card title='Saldo disponible' amount={50000} icon={faWallet} icon_variant='text-[#99A1B7]' variant='bg-[#D8E2FC]' />
+        <Card title='Presupuesto Mensual' amount={200000} icon={faMagnifyingGlassDollar} icon_variant='text-[#A7BCCD]' variant='bg-[#CDE7FC]' />
     </div>
   )
 }
