2375cd71411c1b379ec8785c7a6c0cb85438637b
Jeim05 <lopezvegajeimmy@gmail.com>
2024-11-14 21:46:33 -0600

    flex-basis

--- components/dashboard/card.tsx
+++ components/dashboard/card.tsx
@@ -4,12 +4,12 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 export default function Card({title,amount, icon, icon_variant, variant}: CardProps) {
   return (
-    <div className={`grid grid-flow-row-dense grid-cols-3 py-4 px-5 rounded-lg items-center w-full h-36 ${variant}`}>
-      <div className='col-span-2'>
-        <h3 className='font-bold text-gray-400 py-1'>{title}</h3>
-        <p className='font-bold text-xl'>₡{amount}</p>
+    <div className={`flex flex-row py-4 px-5 rounded-lg items-center w-full h-36 ${variant}`}>
+      <div className='basis-3/4'>
+        <h3 className='font-bold text-gray-400 py-1 mb-4'>{title}</h3>
+        <p className='font-bold text-2xl'>₡{amount}</p>
       </div>
-      <div className={`text-3xl text-end ${icon_variant}`}>
+      <div className={`basis-1/4 text-4xl text-end ${icon_variant}`}>
       <FontAwesomeIcon icon={icon} />
       </div>
     </div>
