b6d39db82473ab84d59039ed51a9adcde64e23e7
Jeim05 <lopezvegajeimmy@gmail.com>
2024-11-15 20:24:40 -0600

    switcher

--- components/ui/DarkModeSwitcher.tsx
+++ components/ui/DarkModeSwitcher.tsx
@@ -6,16 +6,11 @@ export const DarkModeSwitcher = () => {
         <li>
             <label className="relative flex items-center cursor-pointer h-8 w-16 bg-gray-300 dark:bg-gradient-to-r from-blue-500 to-purple-500 rounded-full shadow-inner transition-all">
                 <input type="checkbox" className="sr-only peer" />
-                
-                {/* Contenedor general para los íconos */}
                 <span className="absolute flex items-center justify-between w-full px-1">
-                    {/* Ícono del Sol */}
-                    <span className="flex items-center justify-center h-6 w-6 bg-white rounded-full shadow-md">
-                        <FontAwesomeIcon icon={faSun} className="text-yellow-400" />
+                    <span className="flex items-center justify-center h-6 w-6 bg-white rounded-full shadow-md ">
+                        <FontAwesomeIcon icon={faSun} className='text-gray-400' />
                     </span>
-                    
-                    {/* Ícono de la Luna */}
-                    <span className="dark:flex items-center justify-center h-6 w-6 bg-white rounded-full shadow-md hidden">
+                    <span className="hidden dark:flex items-center justify-center h-6 w-6 bg-white rounded-full shadow-md ">
                         <FontAwesomeIcon icon={faMoon} className="text-blue-500" />
                     </span>
                 </span>
