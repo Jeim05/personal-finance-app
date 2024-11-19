4023a95b1166c1dbfa39cc47ad4efd1a1058b561
Jeim05 <lopezvegajeimmy@gmail.com>
2024-11-18 20:51:28 -0600

    CARD PRESUPUESTO
    Actualización de estlos

--- components/dashboard/CardPresupuesto.tsx
+++ components/dashboard/CardPresupuesto.tsx
@@ -2,16 +2,16 @@ import React from 'react'
 
 export const CardPresupuesto = () => {
     return (
-        <div className='bg-white py-4 px-4 rounded-md basis-1/2'>
+        <div className='budged-card'>
             <h3 className='subtitulos'>
                 Presupuesto mensual
             </h3>
-            <div className='bg-zinc-100 rounded-md px-3 pt-4 my-2'>
+            <div className='budget-amounts-box'>
                 <p className='card-amount py-1'>₡ 260000</p>
                 <hr />
                 <div className='py-1 mt-2'>
                     <p className='text-gray-600 text-sm font-mono'>Monto disponible</p>
-                    <p className='text-gray-700 font-mono'>₡60000</p>
+                    <p className='text-gray-700 font-mono'>₡ 60000</p>
                 </div>
             </div>
         </div>
