88367c9883dcfc73f244042ecdf8d5c2e5661225
Jeim05 <lopezvegajeimmy@gmail.com>
2024-12-11 22:15:06 -0600

    updated

--- components/transactions/TableTransactions.tsx
+++ components/transactions/TableTransactions.tsx
@@ -25,7 +25,21 @@ const TableTransactions = () => {
 
   return (
     <div className="p-4">
-      <DataTable
+      <table className='table '>
+        <thead className='table-header'>
+          <tr>
+            <th className='px-2 py-3'>Categoría</th>
+            <th className='px-2 py-3'>Monto gastado</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr  className='table-item '>
+            <td className='px-2 py-3'>Nombre</td>
+            <td className='px-2 py-3'>₡ 15000</td>
+          </tr>
+        </tbody>
+      </table>
+      {/* <DataTable
         data={data}
         columns={columns}
         options={{
@@ -35,7 +49,7 @@ const TableTransactions = () => {
           responsive: true, 
         }}
         className="table-auto w-full text-sm text-gray-500 border border-gray-200 dark:text-gray-400 dark:border-gray-700"
-      />
+      /> */}
     </div>
   );
 };
