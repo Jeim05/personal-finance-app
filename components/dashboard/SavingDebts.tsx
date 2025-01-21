00be4c68a9db326b770a813d7a5c03c082d2ade0
Jeim05 <lopezvegajeimmy@gmail.com>
2025-01-20 22:53:02 -0600

    Update SavingDebts.tsx

--- components/dashboard/SavingDebts.tsx
+++ components/dashboard/SavingDebts.tsx
@@ -10,16 +10,12 @@ export const SavingDebts = () => {
         amount={500000.0}
         icon={faPiggyBank}
         icon_variant="text-[#7EB5B1]"
-        arrow={faPiggyBank}
-        arrow_variant="text-green-500"
       />
       <Card
         title="Egreso Mensual"
         icon={faSackDollar}
         amount={60000}
         icon_variant="text-[#B25C60]"
-        arrow={faSackDollar}
-        arrow_variant="text-red-500"
       />
     </div>
   )
